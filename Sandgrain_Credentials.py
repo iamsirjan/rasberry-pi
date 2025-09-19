c19af1b3e001b3c18e3ebccea954e5ade7105394
-add <sirjan.gnr@gmail.com>
2025-09-19 17:54:08 +0545

    fix: sandgrain issues

--- Sandgrain_Credentials.py
+++ Sandgrain_Credentials.py
@@ -1,15 +1,15 @@
-uatcloudflaretokens = {'CF-Access-Client-Id': 'be2cbd7f60bd9c3df2fc66729eb3aa69.access', 'CF-Access-Client-Secret' : '8df3182d252444823474930fb30aa983a758c3becd94ba93d822d61b8af80608'}
+#uatcloudflaretokens = {'CF-Access-Client-Id': 'be2cbd7f60bd9c3df2fc66729eb3aa69.access', 'CF-Access-Client-Secret' : '8df3182d252444823474930fb30aa983a758c3becd94ba93d822d61b8af80608'}
 
-sandboxcloudflaretokens = {'CF-Access-Client-Id': '3a9a32e7d40e907c86c8380a14d3c5fe.access', 'CF-Access-Client-Secret' : '35300469289dcb77b5d3b117c354f62ef591bdcc971ac3011760668cb337caa9'}
+sandboxcloudflaretokens = {'CF-Access-Client-Id': '620d727e187e5f3bfafb1d13c3be8c87.access', 'CF-Access-Client-Secret' : 'c76c3d084258ab531fef1217ed962f34cab567be2fefd5774f9634403aa0dc56'}
 
-#cloudflaretokens = sandboxcloudflaretokens
-cloudflaretokens = uatcloudflaretokens
+cloudflaretokens = sandboxcloudflaretokens
+#cloudflaretokens = uatcloudflaretokens
 
-iotusername = 'hsrc-demo'
-iotpassword = 'abcd'
+#iotusername = 'hsrc-demo'
+#iotpassword = 'abcd'
 
-#iotusername = 'jmd-test'
-#iotpassword = 'Cl@imTest1'
+iotusername = 'sgdemo-usb'
+iotpassword = 'P@ss1234'
 
 #tenantusername = 'jeroen.doumen+sandbox@sandgrain.eu'
 #tenantpassword = 'P@ss1234'
